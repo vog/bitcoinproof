08c6549444880ad058cc002ff5f2f3f049b10f5a
Volker Grabsch <vog@notjusthosting.com>
2013-04-05 22:03:18 +0200

    Add "Fork me on GitHub" ribbon

--- lib/update.sh
+++ lib/update.sh
@@ -7,4 +7,7 @@ wget "https://crypto-js.googlecode.com/files/CryptoJS%20v$version.zip" -O Crypto
 unzip -jo CryptoJS.zip rollups/sha256.js rollups/ripemd160.js
 rm -f CryptoJS.zip
 
+# "Fork me on GitHub" ribbon - https://github.com/blog/273-github-ribbons
+wget https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png
+
 echo OK
